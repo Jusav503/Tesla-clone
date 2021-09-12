01cc0b82bc7277221b26dadbcef2d6c06ca3a691
Justice Velasco <justicevgomez001@gmail.com>
2021-09-12 23:52:04 +0200

    styles updated

--- App.js
+++ App.js
@@ -1,18 +1,17 @@
-import { StatusBar } from 'expo-status-bar';
-import React from 'react';
-import { StyleSheet, Text, View } from 'react-native';
+import { StatusBar } from "expo-status-bar";
+import React from "react";
+import { StyleSheet, Text, View } from "react-native";
 
 export default function App() {
   return (
     <View style={styles.container}>
-
       <View style={styles.carContainer}>
         <View style={styles.titles}>
           <Text style={styles.title}>Model S</Text>
-          <Text style={styles.title}>Starting at €65,300</Text>
+          <Text style={styles.subtitle}>Starting at €65,300</Text>
         </View>
       </View>
-        <StatusBar style="auto" />
+      <StatusBar style="auto" />
     </View>
   );
 }
@@ -20,8 +19,12 @@ export default function App() {
 const styles = StyleSheet.create({
   container: {
     flex: 1,
-    backgroundColor: '#fff',
-    alignItems: 'center',
-    justifyContent: 'center',
+    backgroundColor: "#fff",
+    alignItems: "center",
+    justifyContent: "center",
   },
+  carContainer: { width: "100%", height: "100%" },
+  titles: { marginTop: "30%", width: "100%", alignItems: "center" },
+  title:{fontSize:40, fontWeight: "500"},
+  subtitle:{fontSize: 16, color:"#5c5e62"}
 });
